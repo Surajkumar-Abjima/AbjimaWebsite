843e2172317368a6bdd60b82d578cd3cafb9a3e1
SurajKumar <SurajKumar@ABJIMA-SURAJ>
2025-03-24 18:55:21 +0530

    second commit

--- JS/script.js
+++ JS/script.js
@@ -1,7 +1,15 @@
-window.addEventListener("load", function () {
-    document.body.classList.add("loaded"); // Triggers the animation
-});
+
 
 document.querySelector(".hamburger").addEventListener("click", function() {
     document.querySelector(".nav-items").classList.toggle("active");
 });
+let btn=document.querySelector("#toggleBtn")
+let sentance=document.querySelector(".togglesentance")
+btn.addEventListener("click",()=>{
+    if(sentance.style.display=="none"){
+        sentance.style.display="block"
+    }
+    else{
+        sentance.style.display="none"
+    }
+})
