52ca46cfdd8b6241d413c753c0194cf8b71414cd
Suraj Kumar <suraj.kumar@abjima.com>
2025-03-25 18:27:06 +0530

    Final commit

--- JS/script.js
+++ JS/script.js
@@ -1,15 +1,34 @@
-
-
 document.querySelector(".hamburger").addEventListener("click", function() {
-    document.querySelector(".nav-items").classList.toggle("active");
+    document.querySelector(".navs-items").classList.toggle("active");
 });
-let btn=document.querySelector("#toggleBtn")
-let sentance=document.querySelector(".togglesentance")
-btn.addEventListener("click",()=>{
-    if(sentance.style.display=="none"){
-        sentance.style.display="block"
-    }
-    else{
-        sentance.style.display="none"
-    }
-})
+
+let sentance = document.querySelector(".togglesentance");
+let arrow = document.querySelector(".arrow");
+let sentance1 = document.querySelector(".togglesentance1");
+let arrow1 = document.querySelector(".arrow1");
+let sentance2 = document.querySelector(".togglesentance2");
+let arrow2 = document.querySelector(".arrow2");
+
+function closeAll() {
+    arrow.classList.remove("arrowShow");
+    sentance.classList.remove("sentanceShow");
+    arrow1.classList.remove("arrowShow1");
+    sentance1.classList.remove("sentanceShow1");
+    arrow2.classList.remove("arrowShow2");
+    sentance2.classList.remove("sentanceShow2");
+}
+
+function handleToggle() {
+    arrow.classList.toggle("arrowShow");
+    sentance.classList.toggle("sentanceShow");
+}
+
+function handleToggle1() {
+    arrow1.classList.toggle("arrowShow1");
+    sentance1.classList.toggle("sentanceShow1");
+}
+
+function handleToggle2() {
+    arrow2.classList.toggle("arrowShow2");
+    sentance2.classList.toggle("sentanceShow2");
+}
